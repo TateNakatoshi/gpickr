c39a5e3f6a1e0b77e83c0eae668c0455ded03d23
Taco <nunya@biznis.haha>
2020-03-31 22:44:23 -0600

    Add commented destroyAndRemove test in example app

--- st/script.js
+++ st/script.js
@@ -1,7 +1,11 @@
 const gpickr = new GPickr({
-    el: '.gpickr',
+    el: document.querySelector('.gpickr'),
     stops: [
         ['rgb(255,132,109)', 0],
         ['rgb(255,136,230)', 1]
     ]
 });
+
+// setTimeout(() => {
+//     gpickr.destroyAndRemove();
+// }, 4000)
