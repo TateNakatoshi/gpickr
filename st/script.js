6f3f52524550e463e967ecb0a9d60c808f2d8159
Taco <nunya@biznis.haha>
2020-04-01 22:16:11 -0600

    Add toggle gpickr with destroy example

--- st/script.js
+++ st/script.js
@@ -1,12 +1,26 @@
-const gpickr = new GPickr({
-    el: document.querySelector('.gpickr'),
-    stops: [
-        ['rgb(255,132,109)', 0],
-        ['rgb(255,136,230)', 1]
-    ]
-}).on('change', (() => {
-    console.log('Gradient => ', gpickr.getGradient());
-}));
+let gpickr;
+
+gToggleDestroy = () => {
+    if (document.querySelector('.gpickr')) {
+        if (gpickr) {
+            gpickr.destroyAndRemove();
+            gpickr = null;
+        }
+    } else {
+        document.querySelector('.main').innerHTML = '<div class="gpickr"></div>';
+        gpickr = new GPickr({
+            el: document.querySelector('.gpickr'),
+            stops: [
+                ['rgb(255,132,109)', 0],
+                ['rgb(255,136,230)', 1]
+            ]
+        }).on('change', (() => {
+            console.log('Gradient => ', gpickr.getGradient());
+        }));
+    }
+}
+
+document.getElementById ("toggle").addEventListener ("click", gToggleDestroy, false);
 
 // setTimeout(() => {
 //     gpickr.destroyAndRemove();
